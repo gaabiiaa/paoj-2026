93f811e34c06f919f2a9eeb324c49f3a08263553
gaabiiaa <gabiaygan@yahoo.com>
2026-03-19 00:01:31 +0200

    .

--- src/com/pao/laboratory03/exercise/exception/InvalidAgeException.java
+++
@@ -1,7 +0,0 @@
-package com.pao.laboratory03.exceptions;
-
-public class InvalidAgeException extends RuntimeException{
-    public InvalidAgeException(String message) {
-        super(message);
-    }
-}
